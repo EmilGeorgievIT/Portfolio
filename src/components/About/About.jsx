637b81c54dc9b5f034a0f981088ef0bba0de1efe
EmilGeorgievIT <emil_georgiev_it@abv.bg>
2019-11-05 18:13:05 +0000

    Add list-about me details

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,8 +21,9 @@ export default function About() {
                         <div className="container container--small">
                             <div className="section__entry">
                                 <p>
-                                    Hello, I'm Emil.
-                                    I'm a highly talented website developer with experience in building high quality modern and professional sites of various types ( company/organization website, landing page, blog, personal website, eCommerce store, portfolio website ). My knowledge includes: creating site, setting up and installing plugins, fixing bugs, expanding the functionality of the site. I can make a website from scratch or redesign and improve your existing site.
+                                    Hello, I'm Emil. <br/>
+                                    I'm a highly talented website developer with experience in building high quality modern and professional sites of various types ( company/organization website, landing page, blog, personal website, eCommerce store, portfolio website ). <br/>
+                                    My knowledge includes: creating site, setting up and installing plugins, fixing bugs, expanding the functionality of the site. I can make a website from scratch or redesign and improve your existing site.
                                 </p>
                             </div>
                             
@@ -48,6 +49,54 @@ export default function About() {
                                         <a href='mailto:emil_georgiev_it@abv.bg'>
                                             emil_georgiev_it@abv.bg
                                         </a>
+                                    </p>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <strong>
+                                            Address:
+                                        </strong>
+
+                                        <a target="_blank" href='https://www.google.com/maps/place/6+Carrig+Ct,+Fortunestown,+Dublin/data=!4m2!3m1!1s0x4867746724d90917:0x66baef66771ce998?sa=X&ved=2ahUKEwjlwcLE1dPlAhWxqHEKHRVNCT0Q8gEwAHoECBQQAQ'>
+                                            6 Carrig Court, Fortunestown, Dublin
+                                        </a>
+                                    </p>
+                                </li>
+                                
+                                <li>
+                                    <p>
+                                        <strong>
+                                            Phone:
+                                        </strong>
+
+                                        <a target="_blank" href='tel:0833617736'>
+                                            083 361 7736
+                                        </a>
+                                    </p>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <strong>
+                                            Date of birth:
+                                        </strong>
+
+                                        <span>
+                                            6th August 1994
+                                        </span>
+                                    </p>
+                                </li>
+
+                                <li>
+                                    <p>
+                                        <strong>
+                                            Nationality:
+                                        </strong>
+
+                                        <span>
+                                            Bulgaria
+                                        </span>
                                     </p>
                                 </li>
                             </ul>
